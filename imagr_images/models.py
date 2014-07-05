b994ec5eb1ab8458d051ef0b018b3c49444c5b23
corinnelhh <corinne.hutchinson@gmail.com>
2014-07-05 13:33:30 -0700

    attempting to add user model

--- imagr_images/models.py
+++ imagr_images/models.py
@@ -1,43 +1,54 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class ImagrUser(User):
+
+    follows = models.ManyToManyField('self')
+   # followed
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __unicode__(self):
+        return self.username
+
+
 class Photo(models.Model):
+    image = models.ImageField(upload_to='photos/%Y/%m/%d')
+    title = models.CharField(max_length=50)
+    owner = models.ForeignKey(ImagrUser)
+    date_uploaded = models.DateTimeField('date uploaded', auto_now_add=True)
+    date_modified = models.DateTimeField('date modified', auto_now=True)
+    date_published = models.DateTimeField(
+        'date published', blank=True, auto_now=True
+    )
+    access_level = models.IntegerField(choices=(('private', 0), ('public', 1)))
 
     def __unicode__(self):
         return self.title
-
-    image = models.ImageField(upload_to='photos/%Y/%m/%d')
-    title = models.CharField(max_length=50)
-    owner = models.ForeignKey(User)
-    date_uploaded = models.DateTimeField('date uploaded')
-    date_modified = models.DateTimeField('date modified')
-    date_published = models.DateTimeField('date published')
-    access_level = models.IntegerField(choices=(('private', 0), ('public', 1)))
 
 
 class Album(models.Model):
+    owner = models.ForeignKey(ImagrUser)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_modified = models.DateTimeField('date modified', auto_now=True)
+    date_published = models.DateTimeField(
+        'date published', blank=True, auto_now=True
+    )
+    access_level = models.IntegerField(choices=(('private', 0), ('public', 1)))
+    photos = models.ManyToManyField(Photo)
+    cover_photo = models.ForeignKey(
+        Photo, limit_choices_to=(Photo.objects.filter(access_level=1))
+    )
+
+    # def get_photos(self):
+
+    #     return self.photos
 
     def __unicode__(self):
         return self.title
-
-    owner = models.ForeignKey(User)
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    date_created = models.DateTimeField('date created')
-    date_modified = models.DateTimeField('date modified')
-    date_published = models.DateTimeField('date published')
-    access_level = models.IntegerField(choices=(('private', 0), ('public', 1)))
-    photos = models.ManyToManyField(Photo)
-    cover_photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-
-
-class Site_User(models.Model):
-
-    def __unicode__(self):
-        return self.user
-
-    user = models.OneToOneField(User)
-    following = models.ManyToManyField('self', related_name="follows", symmetrical=False)
-    date_joined = models.DateTimeField('date joined')
-    status = models.IntegerField(choices=(('active', 0), ('inactive', 1)))
